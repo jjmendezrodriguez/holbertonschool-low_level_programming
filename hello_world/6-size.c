6b700ceea39b525ddb6518a3ab2401b199c3ac3e
jm <jjmendezrodriguez@yahoo.com>
2024-02-19 14:57:45 -0400

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - starts the program
+ *
+ * Return: 0
+ */
+int main(void) 
+{
+	char one;
+	int two;
+	long int tree;
+	long long int four;
+	float five;
+printf("size of a char: %zu bytes(s)\n",sizeof(char));
+printf("size of a int: %zu bytes(s)\n",sizeof(int));
+printf("size of a long int: %zu bytes(s)\n",sizeof(long int));
+printf("size of a long long int: %zu bytes(s)\n",sizeof(long long int));
+printf("size of a float: %zu byte(s)\n",(float));
+return (0);
+}
