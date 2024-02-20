7375a5b6de8ee61a10a1ec54d17002e6d2252a5b
jm <jjmendezrodriguez@yahoo.com>
2024-02-19 22:18:12 -0400

    fix 6A

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,6 +16,7 @@ printf("size of a char: %zu bytes(s)\n",sizeof(char));
 printf("size of a int: %zu bytes(s)\n",sizeof(int));
 printf("size of a long int: %zu bytes(s)\n",sizeof(long int));
 printf("size of a long long int: %zu bytes(s)\n",sizeof(long long int));
-printf("size of a float: %zu byte(s)\n",(float));
+printf("size of a float: %zu byte(s)\n",sizeof(float));
+
 return (0);
 }
