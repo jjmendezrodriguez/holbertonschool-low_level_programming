1a64c323a4c8c920d617bbfe0e72558758bea883
jm <jjmendezrodriguez@yahoo.com>
2024-02-20 09:22:17 -0400

    fixing spaces

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,15 +7,17 @@
  */
 int main(void)
 {
-char uno;
-int dos;
-long int tres;
-long long int cuatro;
-float cinco;
-printf("size of a char: %zu bytes(s)\n", sizeof(char));
-printf("size of a int: %zu bytes(s)\n", sizeof(int));
-printf("size of a long int: %zu bytes(s)\n", sizeof(long int));
-printf("size of a long long int: %zu bytes(s)\n", sizeof(long long int));
-printf("size of a float: %zu byte(s)\n", sizeof(float));
-return (0);
+	char uno;
+	int dos;
+	long int tres;
+	long long int cuatro;
+	float cinco;
+
+	printf("size of a char: %zu bytes(s)\n", sizeof(char));
+	printf("size of an int: %zu bytes(s)\n", sizeof(int));
+	printf("size of a long int: %zu bytes(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
 }
