2012fc25834bcd2e37823e94a9e0ef162b80c193
jm <jjmendezrodriguez@yahoo.com>
2024-02-21 13:58:50 -0400

    task 7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - starts the program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char letter;
+
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
+putchar('\n');
+
+return (0);
+}
