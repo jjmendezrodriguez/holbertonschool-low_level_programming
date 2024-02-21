f1df01a018ab8817a3037f5074327fb68f6fe6c7
jm <jjmendezrodriguez@yahoo.com>
2024-02-21 11:09:28 -0400

    task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - starts the program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		putchar(letter);
+	}
+		putchar('\n');
+
+	return (0);
+}
