592675b7dcfea4b2c0644f7d782fb2cdea47d3ff
jm <jjmendezrodriguez@yahoo.com>
2024-02-21 12:33:15 -0400

    task 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - starts the program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	
+	printf("0123456789 \n");
+
+return (0);
+}
