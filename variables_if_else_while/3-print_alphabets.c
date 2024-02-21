f542d99b45d64520eed7d056f5c50e7a49c7df4e
jm <jjmendezrodriguez@yahoo.com>
2024-02-20 23:49:54 -0400

    task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - starts the program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+	char upletter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	putchar(letter);
+}
+	for (upletter = 'A'; upletter <= 'Z'; upletter++)
+	{
+	putchar(upletter);
+}
+	putchar('\n');
+
+return (0);
+}
