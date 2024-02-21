baaf3a788430b50d2eb3c7b36bef6afa9552585c
jm <jjmendezrodriguez@yahoo.com>
2024-02-21 13:46:07 -0400

    task 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - starts the program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 48; num <= 57; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
