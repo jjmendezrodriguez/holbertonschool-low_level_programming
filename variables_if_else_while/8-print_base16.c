65a107134fc10f7e8b744c1f08ed1bd736ed04cd
jm <jjmendezrodriguez@yahoo.com>
2024-02-21 14:16:09 -0400

    task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - starts the program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char num;
+
+for (num = '0'; num <= '9'; num++)
+putchar(num);
+
+for (num = 'a'; num <= 'f'; num++)
+putchar(num);
+putchar('\n');
+
+return (0);
+}
