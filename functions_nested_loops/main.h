1afa645323a112673976eb31d0290753a63bb0c1
jm <jjmendezrodriguez@yahoo.com>
2024-02-22 13:33:41 -0400

    task 0

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void print(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+}
