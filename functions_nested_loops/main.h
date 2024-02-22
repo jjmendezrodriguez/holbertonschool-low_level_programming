c699e03a05cd997e1d7352c2e5d50b9290a43493
jm <jjmendezrodriguez@yahoo.com>
2024-02-22 14:31:08 -0400

    fix

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -16,10 +16,10 @@ void print_alphabet(void)
 {
 	char letters;
 
-	for (letters = 'a'; letters <= 'z'; letters++);i
-	{
-	_putchar(letters);
-	}
-	_putchar('\n');
+	for (letters = 'a'; letters <= 'z'; letters++)
+		{
+			_putchar(letters);
+		}
+		_putchar('\n');
 
 }
