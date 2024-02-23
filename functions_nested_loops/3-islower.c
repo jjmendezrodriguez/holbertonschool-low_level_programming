1968ecd0b63af71c7bd810f5fe63048ed167d591
jm <jjmendezrodriguez@yahoo.com>
2024-02-23 12:55:27 -0400

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ *
+ * @c: value to be checked
+ *
+ * Return: 1 when 1 if c lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}	
+	else
+	{
+	return (0);
+	}
+}
