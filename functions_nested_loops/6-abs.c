622373baf28bb4f9a79d9517f9bad80542071cb4
jm <jjmendezrodriguez@yahoo.com>
2024-02-24 00:42:44 -0400

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - stard the program
+ *
+ *@j: value
+ *
+ * Return: Always 0.
+ */
+int _abs(int j)
+{
+	if (j < 0)
+	{
+		return (-j);
+	}
+	else
+	{
+		return (j);
+	}
+}
