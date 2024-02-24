7ffd7b03d5da0940855571c73f49f09d928a9256
jm <jjmendezrodriguez@yahoo.com>
2024-02-24 17:00:29 -0400

    task 10

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - starts the program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char num;
+
+for (num = '0'; num <= '9'; num++)
+{
+        putchar(num);
+        if (num != '9')
+        {
+                putchar(',');
+                putchar(' ');
+        }
+}
+putchar('\n');
+
+return (0);
+}
