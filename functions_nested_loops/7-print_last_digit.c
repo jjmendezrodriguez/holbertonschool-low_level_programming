9dd194e50ffc267b494c55a727746e9e9a83a0ea
jm <jjmendezrodriguez@yahoo.com>
2024-02-24 12:00:48 -0400

    fix 7g

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,13 @@ int print_last_digit(int i)
 {
 	int lastdigit;
 
-	if (i < 0)
-	{
-		i = -i;
-	}
 	lastdigit = i % 10;
-	_putchar(lastdigit);
+
+	if (lastdigit < 0)
+	{
+		lastdigit  = -lastdigit;
+	}
+	_putchar(lastdigit + '0');
 	_putchar('\n');
 
 return (lastdigit);
