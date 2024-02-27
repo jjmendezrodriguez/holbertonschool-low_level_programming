f64e6e7cb3af9bab104aa8f6cd24ce1bccb9ddd8
jm <jjmendezrodriguez@yahoo.com>
2024-02-26 21:46:32 -0400

    task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * sustituimos numbers by text
+ * only en multiples of 3 and 5
+ *
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num < 100; num++)
+	{
+		putchar(' ');
+
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+
+	else if ((num % 3) == 0)
+		printf("Fizz");
+
+		else if ((num % 5) == 0)
+			printf("Buzz");
+	else
+		printf("%d", num);
+	}
+	printf("\n");
+
+	return (0);
+}
