fc2ce0739922bbe92081c430ac59138cd10f3f45
jm <jjmendezrodriguez@yahoo.com>
2024-02-27 13:59:05 -0400

    fix

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * swap_int - cambiar el valor de las integers
- * 
- * @a: valor a
- * @b: valor b
+ * swap_int - swaps values
  *
- * @*: convert to pointers 
+ * @a: value of a
+ *
+ * @b: vaue of b
  */
+
 void swap_int(int *a, int *b)
 {
 	int tmp = *a;
+
 	*a = *b;
 	*b = tmp;
 }
