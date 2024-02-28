899c63e440d786120c360a11a88715ffa83cae2b
jm <jjmendezrodriguez@yahoo.com>
2024-02-28 11:01:29 -0400

    fix 7b

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,8 +9,8 @@ void puts_half(char *str)
 	int len = 0, i = 0, n;
 
 	while (str[len] != '\0')
-	
-	len++;
+
+		len++;
 
 	if ((len % 2) == 0)
 		n = len / 2;
@@ -19,6 +19,6 @@ void puts_half(char *str)
 
 	for (i = n ; i < len; i++)
 		_putchar(str[i]);
-	
+
 	_putchar('\n');
 }
