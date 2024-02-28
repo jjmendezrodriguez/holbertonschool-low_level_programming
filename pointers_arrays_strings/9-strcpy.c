c278271e86a28fb3fe13a8db14eef95757dc0b76
jm <jjmendezrodriguez@yahoo.com>
2024-02-28 14:50:00 -0400

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - code that copy the value
+ *
+ * @dest: valeu of the variable
+ * @src: valeu of the variable
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
