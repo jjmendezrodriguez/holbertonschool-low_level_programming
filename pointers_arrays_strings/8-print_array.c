bfb24ded7e8aae4c9cf5e8c201e725987eb15fc6
jm <jjmendezrodriguez@yahoo.com>
2024-02-28 13:48:21 -0400

    task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -14,8 +14,12 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		printf(", ");
+	
+		if (i == n - 1)
+		continue;
+	
+		printf(",");
+	
 	}
-
 	printf("\n");
 }
