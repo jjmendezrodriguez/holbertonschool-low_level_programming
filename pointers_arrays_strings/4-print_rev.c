f8253e08b9c370a7f47661bcc8bf3e26b30de321
jm <jjmendezrodriguez@yahoo.com>
2024-02-27 22:33:29 -0400

    fix 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,15 @@ void print_rev(char *s)
 {
 	int len = 0, i;
 
-	while (s[i++])
+	while (s[i++] != '\0')
+	{
 		len++;
+	}
 
 	for (i = len - 1; i >= 0; i--)
+	{
 		_putchar(s[i]);
+	}
 
 	_putchar('\n');
 }
