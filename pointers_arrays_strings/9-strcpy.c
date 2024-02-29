b55d92ae55e45ac6a49b2e8e1ac6e5617c3af174
jm <jjmendezrodriguez@yahoo.com>
2024-02-28 21:41:39 -0400

    fix 9b

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int i = 0;
 
-	while (src[index] != '\0')
+	while (src[i] != '\0')
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
+		i++;
 	}
+
+	dest[i] = '\0';
 
 	return (dest);
 }
