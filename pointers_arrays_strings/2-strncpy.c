2ab3509cd8847db3e5bd874d699d144a9b6b4f01
jm <jjmendezrodriguez@yahoo.com>
2024-03-02 18:31:33 -0400

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *
+ * _strncpy - copy the strings and incres the value
+ *
+ * @dest: variable arguments
+ * @src: variable arguments
+ * @n: el numero maximo que copiaremos de src
+ *
+ * Return: al pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, s_len = 0;
+
+	while (src[i++])
+		s_len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = s_len; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
