006b3b20f24f1916c6b7405ed907c9e9fe7752b2
jm <jjmendezrodriguez@yahoo.com>
2024-03-02 19:44:09 -0400

    task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - change all lowercase to uppercase
+ *
+ * @str: Look up!
+ *
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		i++;
+	}
+
+	return (str);
+}
