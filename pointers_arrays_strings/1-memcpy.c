22e30997be339a4a8149de28c71eff0da7fbc62e
jm <jjmendezrodriguez@yahoo.com>
2024-03-04 14:55:27 -0400

    task 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory
+ *
+ * @dest: varieble
+ * @src: variable
+ * @n: variable
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *i = dest;
+	char *J = src;
+
+		while (n-- > 0)
+	{
+		*i++ = *J;
+	}
+
+	return (dest);
+}
