b5874c88b1425d6dd2f4c4884179bfd2987044be
jm <jjmendezrodriguez@yahoo.com>
2024-03-05 12:54:02 -0400

    fix 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,24 +10,27 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0, j = 0, leng = 0;
+	int text = 0;
 
+	for (leng = 0; needle[leng] != '\0'; leng++)
+	{
+	}
+	if (leng == 0)
+		return (haystack);
 	while (haystack[i] != '\0')
 	{
 		if (haystack[i] == needle[0])
 		{
 			for (j = 0; needle[j] != '\0'; j++)
 			{
-				if (needle[j] != haystack[i+j])
+				text = i + j;
+				if (needle[j] != haystack[text])
 					break;
-			}
-			for (leng = 0; needle[leng] != '\0'; leng++)
-			{
 			}
 			if (leng == j)
 				return (&haystack[i]);
 		}
 		i++;
 	}
-
 	return ('\0');
-}	
+}
