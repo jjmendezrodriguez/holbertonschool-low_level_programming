aea87a9f9492142c64cb4314bc952f44cab1a71d
jm <jjmendezrodriguez@yahoo.com>
2024-03-12 14:30:17 -0400

    fix 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,27 +12,36 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concat;
-	int i, sum, j, len1, len2;
+	int i, sum, len1 = 0, len2 = 0;
 
 	if (s1 == NULL && s2 == NULL)
 	{
 		s1 = "";
 		s2 = "";
 	}
-	len1 = _strlen(s1), len2 = _strlen(s2);
+	while (s1[len1])
+	{
+		len1++;
+	}
+	while (s2[len2])
+	{
+		len2++;
+	}
 	sum = len1 + len2 + 1;
-
 	concat = malloc(sizeof(char) * sum);
-	for (i = 0; i <= len1; i++)
+	if (concat == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len1; i++)
 	{
 		concat[i] = s1[i];
 	}
-	for (j = 0; j <= len2; j++)
+
+	for (i = 0; i < len2; i++)
 	{
-		concat[len1 + j] = s2[j];
+		concat[len1 + i] = s2[i];
 	}
-
-	concat[sum] = '\0';
-
-return (concat);
+	return (concat);
 }
