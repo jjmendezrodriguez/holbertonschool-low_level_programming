22c1cbe0b135121048f3be0e1fb6616add51c33d
jm <jjmendezrodriguez@yahoo.com>
2024-03-14 14:09:00 -0400

    task 0

---
+++ structures_typedef/README.md
@@ -0,0 +1,2 @@
+como usar struc sus usos y como funciona
+Tambien como funciona typedef sus usos 
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * struct - ne command 
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* DOG_H */
