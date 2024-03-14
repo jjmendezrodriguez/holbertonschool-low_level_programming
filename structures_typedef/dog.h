2d07898dd1ca35419b0b42276006c3ae0c641966
jm <jjmendezrodriguez@yahoo.com>
2024-03-14 19:31:52 -0400

    fix 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - new command 
+ * struct dog - new command description  
  * @name: Name
  * @age: Age
  * @owner: Owner
@@ -12,3 +12,14 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
