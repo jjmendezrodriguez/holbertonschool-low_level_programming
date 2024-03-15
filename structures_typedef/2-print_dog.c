cb4890453577bd763ffd82748f98b849a8408cf6
jm <jjmendezrodriguez@yahoo.com>
2024-03-15 09:45:50 -0400

    fix 2d

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,8 +8,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		printf("(nil)\n");
-		return;
+	return;
 	}
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
