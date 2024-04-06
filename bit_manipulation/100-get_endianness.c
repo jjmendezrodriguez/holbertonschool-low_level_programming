89a4062e2efcf0fe6cbf7c56c18ac4a337a126b3
jm <jjmendezrodriguez@yahoo.com>
2024-04-06 19:31:12 -0400

    fix task 100

--- bit_manipulation/100-get_endianness.c
+++ bit_manipulation/100-get_endianness.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * get_endianness - this funcion checks the endianness
+ * get_endianness - Determines the endianness of the machine.
  *
- * return: always 0
+ * Return: 0 if the machine is big endian. 1 if little endian.
  */
 int get_endianness(void)
 {
 	unsigned int num = 1;
 	char *ptr = (char *)&num;
-	
+
 	if (*ptr)
 	return (1);
-	
+
 	else
 	return (0);
 }
